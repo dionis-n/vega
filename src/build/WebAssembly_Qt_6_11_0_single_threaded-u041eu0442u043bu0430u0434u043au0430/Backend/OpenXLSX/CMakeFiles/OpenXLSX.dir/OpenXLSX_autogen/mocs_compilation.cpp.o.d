10711898d6c0af2e71278fbd5515b3de8593fef8
Vasily <malakhovstud@gmail.com>
2026-05-06 14:01:57 +0300

    new .gitignore

--- src/build/WebAssembly_Qt_6_11_0_single_threaded-u041eu0442u043bu0430u0434u043au0430/Backend/OpenXLSX/CMakeFiles/OpenXLSX.dir/OpenXLSX_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-Backend\OpenXLSX\CMakeFiles\OpenXLSX.dir\OpenXLSX_autogen\mocs_compilation.cpp.o: \
-  C:\Users\ADMIN\Documents\vega\src\build\WebAssembly_Qt_6_11_0_single_threaded-u041eu0442u043bu0430u0434u043au0430\Backend\OpenXLSX\OpenXLSX_autogen\mocs_compilation.cpp
