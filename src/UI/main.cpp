0a258b2577c3f0204c8452bab3072d382009d3b0
Vasily <malakhovstud@gmail.com>
2026-05-10 02:18:57 +0300

    ADD PWA

--- src/UI/main.cpp
+++ src/UI/main.cpp
@@ -1,7 +1,6 @@
 #include "MainWidget.h"
-
 #include <QApplication>
-
+#include <emscripten.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +8,17 @@ int main(int argc, char* argv[])
 
     app.setOrganizationDomain("https://github.com/danila258/Vega");
     app.setApplicationName("Vega");
+
+    EM_ASM(
+        var link = document.createElement('link');
+        link.rel = 'manifest';
+        link.href = 'manifest.json';
+        document.head.appendChild(link);
+
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('service-worker.js');
+        }
+        );
 
     MainWidget* Vega = new MainWidget();
     Vega->show();
